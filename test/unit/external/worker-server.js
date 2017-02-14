ceddf24bcecd2bd42c2c5e9a2e7d444fad8b69fe
Anandkumar Patel <anand@runnable.com>
2017-02-13 16:03:59 -0800

    100 test coverage

--- test/unit/external/worker-server.js
+++ test/unit/external/worker-server.js
@@ -3,12 +3,16 @@ require('loadenv')()
 const Code = require('code')
 const Lab = require('lab')
 const Promise = require('bluebird')
+const sinon = require('sinon')
 
 const workerServer = require('../../../lib/external/worker-server.js')
+const configApply = require('../../../lib/workers/config.apply.js')
 
 require('sinon-as-promised')(Promise)
 const lab = exports.lab = Lab.script()
 
+const afterEach = lab.afterEach
+const beforeEach = lab.beforeEach
 const describe = lab.describe
 const expect = Code.expect
 const it = lab.it
@@ -29,5 +33,23 @@ describe('worker-server.js unit test', () => {
       expect(workerServer._events).to.be.an.object()
       done()
     })
+
+    describe('_requireWorkers', () => {
+      beforeEach((done) => {
+        sinon.stub(configApply.prototype, 'run')
+        done()
+      })
+
+      afterEach((done) => {
+        configApply.prototype.run.restore()
+        done()
+      })
+
+      it('should define workers correctly', (done) => {
+        workerServer._tasks.get('config.apply')()
+        sinon.assert.calledOnce(configApply.prototype.run)
+        done()
+      })
+    }) // end _requireWorkers
   }) // end module properties
 }) // end worker-server.js unit test
