37bb6a8d044885456112337610ed5c4e6829bf19
Anandkumar Patel <anand@runnable.com>
2017-02-16 14:52:04 -0800

    add unit test

---
+++ test/unit/workers/config.apply.js
@@ -0,0 +1,30 @@
+'use strict'
+const Code = require('code')
+const Lab = require('lab')
+const Promise = require('bluebird')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+
+const Worker = require('workers/config.apply.js')
+
+require('sinon-as-promised')(Promise)
+const lab = exports.lab = Lab.script()
+
+const describe = lab.describe
+const expect = Code.expect
+const it = lab.it
+
+describe('config.apply unit test', () => {
+  const worker = new Worker()
+
+  describe('_stopWorker', () => {
+    it('should throw worker stop error', () => {
+      const testMsg = 'pirate'
+      return Promise.try(() => {
+        worker._stopWorker(new Error(testMsg))
+      })
+      .catch(WorkerStopError, (err) => {
+        expect(err.message).to.contain(testMsg)
+      })
+    })
+  }) // end _removeUsupportedKeys
+})
