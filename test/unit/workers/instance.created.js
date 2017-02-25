135a7f8a9c83be1e6c4536f68f8f62fc5229360b
Anandkumar Patel <anand@runnable.com>
2017-02-25 14:14:35 -0800

    make worker

--- test/unit/workers/instance.created.js
+++ test/unit/workers/instance.created.js
@@ -1,22 +1,54 @@
 'use strict'
 const Lab = require('lab')
 const Promise = require('bluebird')
+const sinon = require('sinon')
 
+const apiClient = require('external/runnable-api-client.js')
+const publisher = require('external/publisher.js')
 const Worker = require('workers/instance.created.js')
 
 require('sinon-as-promised')(Promise)
 const lab = exports.lab = Lab.script()
 
+const afterEach = lab.afterEach
+const beforeEach = lab.beforeEach
 const describe = lab.describe
 const it = lab.it
 
 describe('instance.created unit test', () => {
-  const worker = new Worker()
+  const testId = '123'
+  const worker = new Worker({
+    instanceId: testId
+  })
 
   describe('run', () => {
-    it('should not error', () => {
-      return Promise.try(() => {
-        worker.run()
+    beforeEach((done) => {
+      sinon.stub(apiClient, 'getConfigsForInstance')
+      sinon.stub(publisher, 'publishTask')
+      done()
+    })
+
+    afterEach((done) => {
+      publisher.publishTask.restore()
+      apiClient.getConfigsForInstance.restore()
+      done()
+    })
+
+    it('should call instance apply', () => {
+      const testConfig = {
+        test: 'me'
+      }
+
+      publisher.publishTask.resolves()
+      apiClient.getConfigsForInstance.resolves(testConfig)
+
+      return worker.run()
+      .then(() => {
+        sinon.assert.calledOnce(apiClient.getConfigsForInstance)
+        sinon.assert.calledWith(apiClient.getConfigsForInstance, testId)
+
+        sinon.assert.calledOnce(publisher.publishTask)
+        sinon.assert.calledWith(publisher.publishTask, 'config.assert', testConfig)
       })
     })
   }) // end run
